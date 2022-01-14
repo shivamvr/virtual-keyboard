8e05123a7b77c707381a109cb472648218c7467a
shivam <shivamweb2@gmail.com>
2022-01-14 18:39:43 +0530

    key press preview added

--- index.js
+++ index.js
@@ -15,22 +15,60 @@ const row4data = 'zxcvbnm'
 const row5data = ', .'
 
 // Created a function for assigning key to element
-const assignkeys = (keys,row)=>{
- 
- for(var key=0; key<keys.length; key++){
-    let button = document.createElement('div')
-     button.classList.add('key')
-     button.innerHTML = keys[key]
-     row.appendChild(button)
- }
- row.classList.add('row')
- keyboard.appendChild(row)
- container.appendChild(keyboard)
+
+const assignkeys = (keys, row) => {
+
+    for (var key = 0; key < keys.length; key++) {
+        let button = document.createElement('div')
+        let preview = document.createElement('div')
+        preview.innerHTML = keys[key]
+        preview.classList.add('preview')
+        preview.classList.add('display-none')
+        button.appendChild(preview)
+        button.classList.add('key')
+        button.innerHTML += keys[key]
+        row.appendChild(button)
+    }
+    row.classList.add('row')
+    keyboard.appendChild(row)
+    container.appendChild(keyboard)
 }
+
+const preveiwKey = (key) => {
+        console.log('key:', key)
+        let preview;
+        let element = document.querySelectorAll('.preview');
+        for (var i=0; i<element.length; i++) {
+            if (element[i].textContent == key) {
+                preview = element[i];
+                break;
+            }
+        }
+        preview.classList.remove('display-none')
+        setTimeout((e)=>{
+            preview.classList.add('display-none')
+        },200)
+    }
+
+//  Adding click event on keys
+
+const addClick = ()=>{
+    window.onload = ()=>{
+    const keys = document.querySelectorAll('.key')
+    keys.forEach(key => {
+        key.addEventListener('click',()=>{
+            preveiwKey(key.textContent[0])
+        })
+    });
+   }
+}
+
+addClick()
 
 assignkeys(row1data, row1)
 assignkeys(row2data, row2)
 assignkeys(row3data, row3)
 assignkeys(row4data, row4)
 assignkeys(row5data, row5)
+
 
